b1db8172d27e93df5bd1730314e35e213e574eec
Masanori Ishiwata <mishiwata1015@gmail.com>
2020-07-25 08:19:36 +0900

    Update portfolio data

--- gatsby-simplefolio/gatsby-config.js
+++ gatsby-simplefolio/gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Masanori Ishiwata(@mishiwata1015)`,
+        short_name: `@mishiwata1015`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
